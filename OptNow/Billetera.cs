bdf03faf7723a9d5ae8999454346c26bc37ab17d
danielito96 <daniel_m9603@hotmail.com>
2018-08-20 11:08:16 -0500

    Terminado de picker
    Se crea la clase billetera, se inserta en el picker las opciones con una lista y se muestra en el front

---
+++ OptNow/Billetera.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace OptNow
+{
+    public class Billetera : ContentPage
+    {
+        public string Name { get; set; }
+    }
+}
+
--- OptNow/RequerirPagoPage.xaml.cs
+++ OptNow/RequerirPagoPage.xaml.cs
@@ -7,12 +7,31 @@ namespace OptNow
 {
     public partial class RequerirPagoPage : ContentPage
     {
+        List<Billetera> billeteras;
         public RequerirPagoPage()
         {
             InitializeComponent();
+            IntApp();
         }
 
-        void PickerCuentaBilletera_SelectedIndexChanged(Object sender, Eventargs)
+        void IntApp()
+        {
+            billeteras = new List<Billetera>();
+
+            billeteras.Add(new Billetera { Name = "Xapo" });
+            billeteras.Add(new Billetera { Name = "Coinbase" });
+            billeteras.Add(new Billetera { Name = "Otra" });
+
+            foreach(var billetera in billeteras){
+
+                PickerCuentaBilletera.Items.Add(billetera.Name);
+            }
+
+        }
+
+        void PickerCuentaBilletera_SelectedIndexChanged(Object sender, EventArgs e){
+            
+        }
 
     }
 }
